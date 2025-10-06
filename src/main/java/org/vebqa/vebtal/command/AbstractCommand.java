edad9d5b3230e0f14b7f7a8c01e4c7e914a9aea7
openTAL <kdoerges@opental.org>
2025-10-06 16:52:14 +0200

    add validator for commands

--- src/main/java/org/vebqa/vebtal/command/AbstractCommand.java
+++ src/main/java/org/vebqa/vebtal/command/AbstractCommand.java
@@ -1,5 +1,10 @@
 package org.vebqa.vebtal.command;
 
+import java.lang.annotation.Annotation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vebqa.vebtal.annotations.Keyword;
 import org.vebqa.vebtal.model.CommandType;
 
 public abstract class AbstractCommand implements ICommand {
@@ -10,6 +15,8 @@ public abstract class AbstractCommand implements ICommand {
 	
 	protected CommandType type;
 	
+	public static final Logger logger = LoggerFactory.getLogger(AbstractCommand.class);
+	
 	protected AbstractCommand(String aCommand, String aTarget, String aValue) {
 		this.command = aCommand.trim();
 		this.target = aTarget.trim();
@@ -18,5 +25,22 @@ public abstract class AbstractCommand implements ICommand {
 	
 	public CommandType getType() {
 		return this.type;
-	}	
+	}
+	
+	/**
+	 * Validate target and value input
+	 * 
+	 * @return
+	 */
+	public boolean validate() {
+		Keyword anno = getClass().getAnnotation(Keyword.class);
+		
+		// check for empty spec
+		if (!anno.hintTarget().isEmpty() && this.target.isEmpty()) return false;
+		if (!anno.hintValue().isEmpty() && this.value.isEmpty()) return false;
+		
+		
+
+		return true;
+	}
 }
